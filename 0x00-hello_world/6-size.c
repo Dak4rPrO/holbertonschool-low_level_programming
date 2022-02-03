883d0997846bac78cf8df143c6b8ddf72d96ac15
Mauricio Miranda <4382@holbertonschool.com>
2022-02-03 12:30:13 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - we will display the size of certain files using printf and size
+ * Return: if the program works it will return 0
+*/
+
+int main(void)
+{
+	    printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	    printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	    printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	    return (0);
+}
