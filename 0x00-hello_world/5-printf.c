8349e9e4a3bff443dd0a856936f6d1685d602b5f
Mauricio Miranda <4382@holbertonschool.com>
2022-02-03 11:46:40 -0800

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - we will display a message on the screen using printf
+ * Return: if the program works it will return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
