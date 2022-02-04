f8bf4068044440541ca04afef87652e7b2590d56
Mauricio Miranda <4382@holbertonschool.com>
2022-02-04 11:52:41 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - imprime todos los numeros de un solo dijito
+ * Return: si el cofigo funciona retorna 0
+*/
+
+int main(void)
+{
+	printf("\n 1 2 3 4 5 6 7 8 9");
+	return (0);
+}
