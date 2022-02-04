1ba89a9144f2c0669a2e420ab221f9479bb84d9c
Mauricio Miranda <4382@holbertonschool.com>
2022-02-04 12:11:47 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - imprime todos los numeros de un solo dijito
+ * Return: si el cofigo funciona retorna 0
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = z; ch <= a; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
