17ea0cf84549f6586c4e3ea38b368c5a88c6c7b7
Mauricio Miranda <4382@holbertonschool.com>
2022-02-07 14:58:39 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_last_digit - imprime un el ultimo digito
+ * @n: n es el numero aleatorio a generar
+ * Return: retorna el valor de n
+*/
+
+int _print_last_digit(int n)
+{
+	if(n < 0)
+		n*= -1;
+	_putchar('o' + (n%10));
+	return (n % 10);
+}
