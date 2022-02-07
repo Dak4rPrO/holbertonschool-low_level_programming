5b74eab4dc66a0665df6b825ea18b5ca737f8f92
Mauricio Miranda <4382@holbertonschool.com>
2022-02-07 14:26:30 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - imprime un numero absoluto
+ * @n: n es el numero aleatorio a generar
+ * Return: retorna si el numero es absoluto o no
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
