a1353f97c14336ccb43659c0b228dd08fbedd12b
Mauricio Miranda <4382@holbertonschool.com>
2022-02-07 12:49:04 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - verifica si 
+ *
+ * Return: Always 0.
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{ 
+		return (1);
+	}
+	else  
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - verifica si hay una letra
+ * @c : si
+ * Return: Always 0.
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
