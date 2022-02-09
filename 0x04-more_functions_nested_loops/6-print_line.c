5167c3d150d9412d628ef7bfbcef81ce935e606c
Mauricio Miranda <4382@holbertonschool.com>
2022-02-09 15:43:31 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n : a.
+*/
+void print_line(int n)
+{
+	int times;
+
+	for (times = 0; times < n; times++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
