83770d9c27fc4cede56ae74cdbd89ce325ac33dc
Mauricio Miranda <4382@holbertonschool.com>
2022-02-09 18:23:06 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - imprime "/" en forma de escalera
+ * @n: cantidad de espaciados
+ */
+
+void print_diagonal(int n)
+{
+	int der;
+	int aba;
+
+	if (n > 0)
+	{
+		for (aba = 1; aba <= n; aba++)
+		{
+			for (der = 1; der < aba; der++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
