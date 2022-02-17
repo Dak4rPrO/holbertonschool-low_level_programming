70b73c9ba9b6b328ca46458014ac9db1ac892cae
Dak4rPrO <shinoflow111@gmail.com>
2022-02-17 06:14:04 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,16 +10,17 @@ char *rot13(char *p)
 {
 	int i = 0;
 	int j = 0;
-	char let[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
-	char rot[] = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
+	char let[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (; p[i]; i++)
 	{
-		for (j = 0; j < 54; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (p[i] == let[j])
 			{
 				p[i] = rot[j];
+				break;
 			}
 		}
 	}
