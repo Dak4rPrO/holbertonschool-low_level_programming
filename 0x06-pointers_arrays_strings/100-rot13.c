478079c1cd1616c3050501a2170e61519246c20a
Dak4rPrO <shinoflow111@gmail.com>
2022-02-17 06:03:00 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * leet - funcion que cambia letras especificas por numeros
+ * rot13 - funcion que cambia letras especificas por numeros
  * @p: puntero
  * Return: retorna el valor del puntero p
  */
 
 char *rot13(char *p)
 {
-        int i = 0;
-        int j = 0;
-        char let[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
-        char rot[] = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
+	int i = 0;
+	int j = 0;
+	char let[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
+	char rot[] = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
 
-        for (; p[i]; i++)
-        {
-                for (j = 0; j < 54; j++)
-                {
-                        if (p[i] == let[j])
-                        {
-                                p[i] = rot[j];
-                        }
-                }
-        }
-        return (p);
+	for (; p[i]; i++)
+	{
+		for (j = 0; j < 54; j++)
+		{
+			if (p[i] == let[j])
+			{
+				p[i] = rot[j];
+			}
+		}
+	}
+	return (p);
 }
