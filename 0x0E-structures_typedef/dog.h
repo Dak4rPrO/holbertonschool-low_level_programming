eee5f3c4db8a40a5370345ae830a40472b3132fb
Dak4rPrO <shinoflow111@gmail.com>
2022-03-08 14:00:13 -0800

    dog.h.08/03

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure
+ * @name: name
+ * @owner: owner
+ * @age: age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+
+dog;
+
+#endif
