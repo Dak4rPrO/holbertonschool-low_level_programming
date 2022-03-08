517ce44b236948da439d891439269a80a7f44886
Dak4rPrO <shinoflow111@gmail.com>
2022-03-08 10:26:09 -0800

    2-main.c.08/03

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - program that prints the name of the file it was compiled from
+* Return: return 0
+*/
+
+int main(void)
+{
+    printf(__FILE__"\n");
+    return 0;
+}
