53c45f5e3d74281f4ad0aed3acb8640c4334552e
Dak4rPrO <shinoflow111@gmail.com>
2022-03-08 18:40:55 -0800

    2-print_dog.c.08/03

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,14 @@ struct dog
 
 dog;
 
+/**
+*dog_t - new name for the type struct dog.
+*/
+typedef struct dog dog_t;
+
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
