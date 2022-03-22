d4a801979fbd44c14f65ac3fd4be894bacd371cf
Dak4rPrO <shinoflow111@gmail.com>
2022-03-22 11:57:31 -0700

    probando

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,6 +9,9 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	
+	if (head == NULL)
+		return;
 
 	while (*head != NULL)
 	{
@@ -16,4 +19,5 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(temp);
 	}
+	*head = NULL;
 }
