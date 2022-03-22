8992527ac1378d8ac793e287031173000414e8ee
Dak4rPrO <shinoflow111@gmail.com>
2022-03-22 09:41:13 -0700

    probando pushear

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,11 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	unsigned int count = 0;
 
 	while (h != NULL)
 	{
-	printf("%d\n", h->n);
+	printf("%u\n", h->n);
 	h = h->next;
 	count++;
 	}
