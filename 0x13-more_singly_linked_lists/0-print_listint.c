9e00610b08899e6a0a7841ccbb1f7ef4daaa9d2a
Dak4rPrO <shinoflow111@gmail.com>
2022-03-22 08:57:55 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,11 @@
 
 size_t print_listint(const listint_t *h)
 {
-	unsigned int count = 0;
+	int count = 0;
 
 	while (h != NULL)
 	{
-	printf("%u\n", h->n);
+	printf("%d\n", h->n);
 	h = h->next;
 	count++;
 	}
