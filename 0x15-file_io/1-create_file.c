41c7b788e1effbb679b25a4b213e24cb106eced4
Dak4rPrO <shinoflow111@gmail.com>
2022-03-28 14:27:13 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,32 +4,31 @@
 * create_file - function that creates a file.
 * @filename: name of the file to create.
 * @text_content: is a NULL terminated string to write to the file.
+* Return: Returns: 1 on success, -1 on failure.
 */
 
 int create_file(const char *filename, char *text_content)
 {
-        ssize_t rd, wt, op;
+	int num, i = 0;
 
-        char *buf = malloc(sizeof(char) * letters);
+	if (filename == NULL)
+		return (-1);
 
-        if (filename == NULL)
-                return (0);
+	if (text_content == NULL)
+		text_content = "";
 
-        if (buf == NULL)
-                return (0);
+	num = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-        op = open(filename, O_RDWR | O_CREAT | O_APPEND);
-        rd = read(op, buf, letters);
-        wt = write(STDOUT_FILENO, buf, rd);
+	i = strlen(text_content);
 
-        if (op == -1 || rd == -1 || wt == -1 || wt != rd)
-        {
-                free(buf);
-                return (0);
-        }
+	write(num, text_content, i);
 
-        free(buf);
-        close(op);
+	if (num == -1)
+	{
+		return (-1);
+	}
 
-        return (wt);
+	close(num);
+
+	return (1);
 }
