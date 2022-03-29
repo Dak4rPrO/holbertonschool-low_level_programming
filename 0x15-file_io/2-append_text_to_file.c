bb7de19160d56d01b059df75a73ee887225da383
Dak4rPrO <shinoflow111@gmail.com>
2022-03-29 06:32:14 -0700

    probando 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,20 +8,28 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int num, i = 0;
+	int num, w, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
 
 	num = open(filename, O_RDWR | O_APPEND);
 
 	if (num == -1)
 		return (-1);
 
-	if (filename == NULL)
-		return (-1);
-
  	i = strlen(text_content);
 	
-	if (text_content == NULL)
-		write(num, text_content, i);
+	w = write(num, text_content, i);
+
+	if (w == -1)
+	{
+		close(num);
+		return (-1);
+	}
 
 	close(num);
 
