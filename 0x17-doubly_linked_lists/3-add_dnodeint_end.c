5c29e254b4e0847eb05b98ef4a88ad0f6399184f
Dak4rPrO <shinoflow111@gmail.com>
2022-05-05 16:12:16 -0700

    probando

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode;
+
+	newNode = malloc(sizeof(dlistint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+
+}
