70f0a4a56804acdbeb460bd809c00456c4bb6888
Dak4rPrO <shinoflow111@gmail.com>
2022-05-09 09:14:38 -0700

    probado

--- 0x18-dynamic_libraries/functions-4.c
+++ 0x18-dynamic_libraries/functions-4.c
@@ -93,3 +93,25 @@ char *_strstr(char *haystack, char *needle)
 	}
 	return ('\0');
 }
+
+/**
+ * _strcat - codigo para conectar 2 cadenas
+ * @dest: cadena "Hello "
+ * @src: cadena "World\n"
+ * Return: retorna dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int idx_dest, idx_src;
+
+	for (idx_dest = 0; dest[idx_dest] != '\0';)
+		idx_dest++;
+
+	for (idx_src = 0; src[idx_src] != '\0'; idx_src++)
+	{
+	dest[idx_src + idx_dest] = src[idx_src];
+	}
+	dest[idx_src + idx_dest] = '\0';
+	return (dest);
+}
