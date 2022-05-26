9b9e762e98204c99123e52a9999f4953e6f95828
Dak4rPrO <shinoflow111@gmail.com>
2022-05-26 11:02:55 -0700

    todo

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - hash table
+ * @size: size is the size of the array
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	
+	if (table == NULL)
+		return (NULL);
+	
+	table->array = malloc(size);
+
+	return (table);
+}
