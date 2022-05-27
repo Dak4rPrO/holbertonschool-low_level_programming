e7c601bb9f5bdcd083be05ae22bd711a9e2bf8d3
Dak4rPrO <shinoflow111@gmail.com>
2022-05-27 13:59:09 -0700

    arreglos betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,6 +3,7 @@
 /**
  * hash_table_create - hash table
  * @size: size is the size of the array
+ * Return: always table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
@@ -10,10 +11,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
-	
+
 	if (table == NULL)
 		return (NULL);
-	
+
 	table->array = malloc(size);
 
 	return (table);
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,6 +3,8 @@
 /**
  * key_index - function that gives you the index of a key.
  * @size: is the size of the array of the hash table
+ * @key: is the key
+ * Return: index at which the key pair should be stored in the array of the h.t
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
