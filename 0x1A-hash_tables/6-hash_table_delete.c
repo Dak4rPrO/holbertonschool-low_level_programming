d0c06abb9ec894350d34532417580086eaab3e8d
Mauricio Miranda <98028958+Dak4rPrO@users.noreply.github.com>
2022-06-26 21:37:36 -0300

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,10 +1,12 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_delete - delete a hash table
  * @ht: hash table
  *
  * Return: void
  */
+
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i = 0;
