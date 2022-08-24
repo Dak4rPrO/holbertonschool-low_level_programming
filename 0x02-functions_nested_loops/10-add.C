c42bd3670a697347ba308d56b3803cefb2ba4d5e
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-08-24 10:07:22 -0300

    Update 10-add.C

---
+++ 0x02-functions_nested_loops/10-add.C
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - sumar "x" con "y" e imprime su resultado
+ *@x: numero x
+ *@y: numero y
+ * Return: retornara el resultado de la suma de "x" e "y"
+*/
+
+int add(int x, int y)
+{
+		return (x + y);
+}
