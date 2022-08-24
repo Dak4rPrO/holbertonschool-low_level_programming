fac0cbdc25cd62f0aee05504e00d73583ca35ca0
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-08-24 12:27:25 -0300

    terminar 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,7 +14,9 @@ int linear_search(int *array, size_t size, int value)
     if (array == NULL)
         return (-1);
 
-    for (i = 0; i < size; i++)
+    for (; i < size; i++)
         if (array[i] == value)
-            printf("Found %d at index: %d\n", i, array[i]);
+            printf("Value checked array [%d] = [%d]\n", i, array[i]);
+
+    return (1);
 }
