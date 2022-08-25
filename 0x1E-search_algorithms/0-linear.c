f1b899afa1acf2ad6e33118186f67ac2725e1072
Dak4rPrO <4382@holbertonstudents.com>
2022-08-25 14:25:46 -0700

    quito la comprobacion del size)

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,7 +13,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t index = 0;
 
-	if (array == NULL || size == '\0')
+	if (array == NULL)
 		return (-1);
 
 	for (; index < size; index++)
--- 0x1E-search_algorithms/0-main.c
+++ 0x1E-search_algorithms/0-main.c
@@ -17,6 +17,5 @@ int main(void)
     printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
     printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
     printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
-    printf("Found %d at index: %d\n", 10101010, linear_search(array, size, 10101010));
     return (EXIT_SUCCESS);
 }
